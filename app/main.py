fa0f52881fc05d2e46a2f1ca67715f9abd5a33d1
mihir <mihir.sangh@gmail.com>
2025-06-02 15:08:35 +0530

    adding front end

--- app/main.py
+++ app/main.py
@@ -3,8 +3,21 @@ from typing import List
 import pandas as pd
 from app.scorer import score_resume
 from app.parser import extract_text_from_file
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app=FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 👈 You can restrict this later to ["http://localhost:3000"] etc.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/upload")
 async def upload_files(jd:UploadFile= File(...), resumes:List[UploadFile]=File(...)):
